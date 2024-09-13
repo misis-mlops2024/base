d78bd049035f872fc749fe6920f8f857c5358708
l3lush <ananasnya@gmail.com>
2024-09-13 16:28:18 +0300

    initial commit

---
+++ sum_script.py
@@ -0,0 +1,13 @@
+def sum_of_two_numbers(num1, num2):
+    return num1 + num2 
+
+
+def main():
+    num1 = 1
+    num2 = 2
+    result = sum_of_two_numbers(num1, num2)
+    print(result)
+    
+
+if __name__ == '__main__':
+    main()
