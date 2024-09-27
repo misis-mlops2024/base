2bf3a8acac3cee6b1c093fa4886b17f295bb9d71
l3lush <ananasnya@gmail.com>
2024-09-27 17:22:43 +0300

    last exp

--- src/dataset.py
+++ src/dataset.py
@@ -4,7 +4,7 @@ import sklearn.datasets
 import sklearn.model_selection
 import typer
 import sklearn
-import pandas as pd 
+import pandas as pd
 import numpy as np
 from loguru import logger
 from tqdm import tqdm
@@ -19,21 +19,25 @@ def main(params_path: str):
     Function to generate dataset
     """
     params = read_pipeline_params(params_path)
-    X, y = sklearn.datasets.make_classification(n_samples=params.data_params.n_samples, n_features=params.data_params.n_features)
+    X, y = sklearn.datasets.make_classification(
+        n_samples=params.data_params.n_samples, n_features=params.data_params.n_features
+    )
     data = pd.DataFrame(np.hstack([X, y.reshape(-1, 1)]))
     data.columns = [f"feat_{i}" for i in range(data.shape[-1])]
     data = data.rename({f"feat_{data.shape[-1] - 1}": "target"}, axis=1)
     logger.info(f"Got data with shape: {data.shape}")
-    
-    train, test = sklearn.model_selection.train_test_split(data, test_size=params.data_params.test_size, random_state=params.random_state)
+
+    train, test = sklearn.model_selection.train_test_split(
+        data, test_size=params.data_params.test_size, random_state=params.random_state
+    )
     logger.info(f"Split data into train ({train.shape}) and test ({test.shape})")
-    
+
     train.to_csv(params.data_params.train_data_path, index=False)
     logger.info(f"Save train sample to the path: {params.data_params.train_data_path}")
-    
+
     test.to_csv(params.data_params.test_data_path, index=False)
     logger.info(f"Save test sample to the path: {params.data_params.test_data_path}")
-    
-    
+
+
 if __name__ == "__main__":
     app()
--- src/modeling/train.py
+++ src/modeling/train.py
@@ -21,29 +21,29 @@ def main(params_path: str):
     train = pd.read_csv(params.data_params.train_data_path)
     X_train = train.drop("target", axis=1)
     y_train = train["target"].values.reshape(-1, 1)
-    
+
     test = pd.read_csv(params.data_params.test_data_path)
     X_test = test.drop("target", axis=1)
     y_test = test["target"].values.reshape(-1, 1)
-    
+
     model = sklearn.ensemble.RandomForestClassifier(n_estimators=params.train_params.n_estimators)
     model.fit(X_train, y_train)
     logger.info(f"Learn model {model}")
-    
+
     y_test_pred = model.predict_proba(X_test)[:, 1]
     roc_auc = sklearn.metrics.roc_auc_score(y_test, y_test_pred)
     logger.info(f"Got ROC-AUC {roc_auc:.3f}")
-    
+
     metrics = {"roc-auc": roc_auc}
-    
-    with open(params.train_params.model_path, 'wb') as fin:
+
+    with open(params.train_params.model_path, "wb") as fin:
         pickle.dump(model, fin)
     logger.info(f"Saved model to path {params.train_params.model_path}")
-    
-    with open(params.train_params.metrics_path, 'w') as fin:
+
+    with open(params.train_params.metrics_path, "w") as fin:
         json.dump(metrics, fin)
     logger.info(f"Saved metrics to path {params.train_params.metrics_path}")
-    
+
 
 if __name__ == "__main__":
     app()
